7dbed822ab899defb5cd2e01ed97ec459686de0b
Fabian Rizzi <faberizzi92@gmail.com>
2023-01-16 15:36:29 +0100

    sequelize syncing

--- api/src/db.js
+++ api/src/db.js
@@ -30,11 +30,18 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Pokemon } = sequelize.models;
+const { pokemon, type } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
 
+
+
+//synchronizing models with database
+async () => {
+  await pokemon.sync();
+  await type.sync();
+}
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
