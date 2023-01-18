8f5c441fc97726c8deb877022ec47be7e05905f2
Fabian Rizzi <faberizzi92@gmail.com>
2023-01-18 12:39:10 +0100

    model now uses uuid instead of integer

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,25 +1,28 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, Sequelize } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
 
   sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.UUID,
+      // autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+      defaultValue: DataTypes.UUIDV4,
+
+      // validate: {
+      //   isNumeric: true,
+      //   doesNotExistInPokeApi: function(value) {
+      //     //validating function 
+      //   }
+      // }
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
-    },
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      unique: true,
-      validate: {
-        isNumeric: true,
-        doesNotExistInPokeApi: function(value) {
-          //validating function 
-        }
-      }
     },
     hp: {
       type: DataTypes.INTEGER,
@@ -33,13 +36,20 @@ module.exports = (sequelize) => {
     spd: {
       type: DataTypes.INTEGER,
     },
-
     height: {
       type: DataTypes.FLOAT,
     },
     weight: {
       type: DataTypes.FLOAT,
     },
-  });
+    type1: {
+      type: DataTypes.STRING,
+    },
+    type2: {
+      type: DataTypes.STRING,
+    },
+    
+  }, { timestamps: false }
+  );
   };
 
