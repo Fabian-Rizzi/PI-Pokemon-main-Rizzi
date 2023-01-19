591b3279586ff5e257c56123c9dc7bbfbd6a177c
Fabian Rizzi <faberizzi92@gmail.com>
2023-01-19 16:23:51 +0100

    now renders home

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 import React from 'react';
 import { Route } from 'react-router-dom';
 import LandingPage from './components/landingPage/landingPage';
+import Home from './components/home/home'
+
 
 function App() {
   return (
     <div className="App">
       <Route exact path="/">
         <LandingPage />
+      </Route>
+
+      <Route exact path="/home">
+        <Home />
       </Route>
     </div>
   );
