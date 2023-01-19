81b9c34f79fa7848c4277b6eac4c9ea6d5a78fc1
Fabian Rizzi <faberizzi92@gmail.com>
2023-01-19 20:34:02 +0100

    now links with Link instead of <a>

--- client/src/components/landingPage/landingPage.jsx
+++ client/src/components/landingPage/landingPage.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import './landingPage.css';
 import logo from './pokeball.png';
+import { Link } from 'react-router-dom';
 import background from './background.jpeg';
 
 export default function LandingPage() {
   return (
     <div className="landing-page" style={{ backgroundImage: `url(${background})` }}>
       <img src={logo} alt="Logo" className="logo" />
-      {/* make a button with className "enter-button", text Enter, that goes to "/home" */}
-      <a href="/home" >
+      <Link to="/home">
         <button className="enter-button" type="button">Enter</button>
-      </a>
+      </Link>
     </div>
   );
 }
