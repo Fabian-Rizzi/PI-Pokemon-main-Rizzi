24c9c19d628f1d950ff5bc67c9cdf2d9ac813c94
Fabian Rizzi <faberizzi92@gmail.com>
2023-01-19 20:34:30 +0100

    variable names corrections and new imports

--- client/src/components/home/home.jsx
+++ client/src/components/home/home.jsx
@@ -3,40 +3,55 @@ import { useState, useEffect } from 'react'
 import './home.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { getPokemons } from '../../actions/actions.js'
+import { Link } from 'react-router-dom'
+import { getById } from '../../actions/actions.js'
+import { getTypes } from '../../actions/actions.js'
+import {searchPokemon} from '../../actions/actions.js'
+import Card from '../card/card.jsx'
+
 
 export default function Home() {
-  const [pokemons, setPokemons] = useState(false)
   const dispatch = useDispatch()
-  const pokemonsRedux = useSelector((state) => state.pokemons) //.pokemons?
+  const [pokemons, setPokemons] = useState(false)
+  const allPokemons = useSelector((state) => state.pokemons) //.pokemons?
   
-  console.log(pokemonsRedux)
+
+  console.log(allPokemons)
   console.log('asd')
 
 
   useEffect(() => {
-    if (pokemons === false) {
-      setPokemons(true)
-    }
-    if (pokemonsRedux.length === 0) {
-      dispatch(getPokemons())
-    }
+    dispatch(getTypes());
+    dispatch(getPokemons());
+  }, []);
 
-  }, [pokemonsRedux])
+  function handleClick(e) {
+    e.preventDefault();
+    dispatch(getPokemons());
+  }
+    
+  
 
+// if (pokemons === false) {
+//   setPokemons(true)
+// }
+// if (allPokemons.length === 0) {
+//   dispatch(getPokemons())
+// }
 
   return (
     <div>
-      <h1>Homeee</h1>
+      <h1>Homeee. Tiembla Pol</h1>
       <div className="container">
-        {pokemonsRedux.map((pokemon) => {
+        {allPokemons.map((pokemon) => {
           return (
             <div className="pokemon-card">
               <h3>{pokemon.name}</h3>
               <div className="pokemon-card-info">
                 <p>HP: {pokemon.hp}</p>
-                <p>Attack: {pokemon.attack}</p>
-                <p>Defense: {pokemon.defense}</p>
-                <p>Speed: {pokemon.speed}</p>
+                <p>Attack: {pokemon.atk}</p>
+                <p>Defense: {pokemon.def}</p>
+                <p>Speed: {pokemon.spd}</p>
                 <p>Height: {pokemon.height}</p>
                 <p>Weight: {pokemon.weight}</p>
               </div>
