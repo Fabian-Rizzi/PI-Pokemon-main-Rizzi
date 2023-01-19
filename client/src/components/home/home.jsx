91798cdb4d5fd7f813e761c6d29f161f2739bd12
Fabian Rizzi <faberizzi92@gmail.com>
2023-01-19 17:01:47 +0100

    home update, card creation

--- client/src/components/home/home.jsx
+++ client/src/components/home/home.jsx
@@ -1,36 +1,50 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import './home.css'
-
-
+import { useDispatch, useSelector } from 'react-redux'
+import { getPokemons } from '../../actions/actions.js'
 
 export default function Home() {
-  const [pokemons, setPokemons] = useState([])
+  const [pokemons, setPokemons] = useState(false)
+  const dispatch = useDispatch()
+  const pokemonsRedux = useSelector((state) => state.pokemons) //.pokemons?
+  
+  console.log(pokemonsRedux)
+  console.log('asd')
+
 
   useEffect(() => {
-    fetch('http://localhost:3002/pokemons')
-      .then((res) => res.json())
-      .then((data) => {
-        setPokemons(data)
-      })
-  }, [])
+    if (pokemons === false) {
+      setPokemons(true)
+    }
+    if (pokemonsRedux.length === 0) {
+      dispatch(getPokemons())
+    }
+
+  }, [pokemonsRedux])
 
 
   return (
     <div>
       <h1>Homeee</h1>
       <div className="container">
-        {pokemons.map((pokemon) => (
-          <div className="card" key={pokemon.id}>
-            <div className="info">
+        {pokemonsRedux.map((pokemon) => {
+          return (
+            <div className="pokemon-card">
               <h3>{pokemon.name}</h3>
-              <p>{pokemon.type}</p>
-
-    </div>
-          </div>
-        ))}
+              <div className="pokemon-card-info">
+                <p>HP: {pokemon.hp}</p>
+                <p>Attack: {pokemon.attack}</p>
+                <p>Defense: {pokemon.defense}</p>
+                <p>Speed: {pokemon.speed}</p>
+                <p>Height: {pokemon.height}</p>
+                <p>Weight: {pokemon.weight}</p>
+              </div>
+            </div>
+          )
+        }
+        )}
       </div>
     </div>
-
   )
 }
