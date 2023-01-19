c1b7026e8842c77e593fe06efec1a79d09dcb210
Fabian Rizzi <faberizzi92@gmail.com>
2023-01-19 20:35:11 +0100

    now using switch and browser router

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, BrowserRouter, Switch} from 'react-router-dom';
 import LandingPage from './components/landingPage/landingPage';
 import Home from './components/home/home'
 
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
+      <Switch>
       <Route exact path="/">
         <LandingPage />
       </Route>
@@ -15,7 +17,9 @@ function App() {
       <Route exact path="/home">
         <Home />
       </Route>
+      </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
