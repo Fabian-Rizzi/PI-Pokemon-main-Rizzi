8ff19530309657daea97b6bdab628b0ff889b069
Fabian Rizzi <faberizzi92@gmail.com>
2023-01-19 17:00:34 +0100

    get by id, get types, search pokemon actions

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-export const GET_POKEMONS = "GET_POKEMONS";
+export const GET_POKEMONS = "GET_POKEMONS",
+    GET_BY_ID = "GET_BY_ID",
+    GET_TYPES = "GET_TYPES",
+    SEARCH_POKEMON = "SEARCH_POKEMON";
+
 
 export function getPokemons() {
   return async function (dispatch) {
@@ -11,4 +15,41 @@ export function getPokemons() {
     });
   };
 }
+
+export function getById(id) {
+    return async function (dispatch) {
+        const response = await axios.get(`http://localhost:3001/pokemons/${id}`);
+        return dispatch({
+        type: GET_BY_ID,
+        payload: response.data,
+        });
+    };
+    }
+
+export function getTypes() {
+    return async function (dispatch) {
+        const response = await axios.get("http://localhost:3001/types");
+        return dispatch({
+        type: GET_TYPES,
+        payload: response.data,
+        });
+    };
+    }
+
+export function searchPokemon(name) {
+    return async function (dispatch) {
+        try {
+        const response = await axios.get(
+            `http://localhost:3001/pokemons?name=${name}`
+        );
+        return dispatch({
+            type: SEARCH_POKEMON,
+            payload: response.data,
+        });
+        } catch (err) {
+        console.log(err.message);
+        console.log("Pokemon not found")
+        }
+    };
+    }
 
