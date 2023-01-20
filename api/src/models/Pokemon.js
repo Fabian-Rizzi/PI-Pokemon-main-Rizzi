cf4fad20f900cca21929c80108c076b9ffa568da
Fabian Rizzi <faberizzi92@gmail.com>
2023-01-20 19:32:27 +0100

    lowercase and validate in name, added img and multiple allownNull

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -23,33 +23,51 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
+      validate: {
+        isAlphanumeric: true,
+      },
+      get() {
+        const rawName = this.getDataValue("name");
+        const rawNameLower = rawName.toLowerCase();
+        return rawName ? rawNameLower : null;
+      },
     },
     hp: {
       type: DataTypes.INTEGER,
+      allowNull: true,
     },
     atk: {
       type: DataTypes.INTEGER,
+      allowNull: true,
     },
     def: {
       type: DataTypes.INTEGER,
+      allowNull: true,
     },
     spd: {
       type: DataTypes.INTEGER,
+      allowNull: true,
     },
     height: {
       type: DataTypes.FLOAT,
+      allowNull: true,
     },
     weight: {
       type: DataTypes.FLOAT,
+      allowNull: true,
     },
     type1: {
       type: DataTypes.STRING,
     },
     type2: {
       type: DataTypes.STRING,
+      allowNull: true,
     },
-    
+    img: {
+      type: DataTypes.STRING,
+      defaultValue:
+        "https://art.pixilart.com/738950d3f7c2a97.png",
+    },
   }, { timestamps: false }
   );
   };
-
