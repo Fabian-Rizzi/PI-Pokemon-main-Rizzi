7646aabddad1b2175da422d7d801e98aac3acf07
Fabian Rizzi <faberizzi92@gmail.com>
2023-01-20 19:52:53 +0100

    navbarc reation

---
+++ client/src/components/navBar/navBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function navBar() {
+  return (
+    <div>
+        <img src="https://seeklogo.com/images/P/Pokemon-logo-497D61B223-seeklogo.com.png" alt="navbar" />
+    </div>
+  )
+}
+
+export default navBar
