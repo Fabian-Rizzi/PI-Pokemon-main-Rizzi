b1c6bbe6bf32e4a81edc51d7479b72ad63fbb5ce
Fabian Rizzi <faberizzi92@gmail.com>
2023-01-20 19:53:05 +0100

    landing now uses navlink for future active link

--- client/src/components/landingPage/landingPage.jsx
+++ client/src/components/landingPage/landingPage.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import './landingPage.css';
 import logo from './pokeball.png';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import background from './background.jpeg';
 
 export default function LandingPage() {
   return (
     <div className="landing-page" style={{ backgroundImage: `url(${background})` }}>
       <img src={logo} alt="Logo" className="logo" />
-      <Link to="/home">
+      <NavLink to="/home">
         <button className="enter-button" type="button">Enter</button>
-      </Link>
+      </NavLink>
     </div>
   );
 }
