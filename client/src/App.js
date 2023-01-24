5a348ea1eb581e6f0aaf71f00f26c45532b8aff5
Fabian Rizzi <faberizzi92@gmail.com>
2023-01-24 12:03:15 +0100

    /home/:id component display

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import React from 'react';
 import { Route, BrowserRouter, Switch} from 'react-router-dom';
 import LandingPage from './components/landingPage/landingPage';
 import Home from './components/home/home'
-
+import { useDispatch, useSelector } from "react-redux";
+import Card from './components/card/card';
 
 function App() {
   return (
@@ -16,6 +17,9 @@ function App() {
 
       <Route exact path="/home">
         <Home />
+      </Route>
+      <Route exact path="/home/:idPokemon">
+        <Card />
       </Route>
       </Switch>
     </div>
