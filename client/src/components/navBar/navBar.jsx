669d2709d6ce67327db439bc0d9802a524d9b42e
Fabian Rizzi <faberizzi92@gmail.com>
2023-01-24 12:06:30 +0100

    navbar edit

--- client/src/components/navBar/navBar.jsx
+++ client/src/components/navBar/navBar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import SearchBar from "../searchBar/searchBar";
+import { NavLink } from "react-router-dom";
 
 function navBar() {
   return (
+    
     <div>
-        <img src="https://seeklogo.com/images/P/Pokemon-logo-497D61B223-seeklogo.com.png" alt="navbar" />
+                <NavLink to="/home">
+          <img
+            src="https://freepngimg.com/thumb/pokemon/20045-9-pokemon-ash-transparent.png"
+            alt="pokedex"
+          />
+        </NavLink>
+
+            <SearchBar />
+        
     </div>
   )
 }
